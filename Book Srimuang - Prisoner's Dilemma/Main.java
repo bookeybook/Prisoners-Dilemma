5554a30235aa8cab88e34052f07392a644444069
Book <57215773+bookeybook@users.noreply.github.com>
2025-06-11 11:01:38 +1200

    Add files via upload
    Version 1

---
+++ Book Srimuang - Prisoner's Dilemma/Main.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Main here.
+ *  This program is a prisonners dilemma game, the aim of this game is too get more points than the AI by the end of the rounds. 
+ *  The player can either pick cooperate or defect and based on what the AI has chosen on the same round the player will either gain or lose points.
+ * @author Book Srimuang
+ * @version Version 1, 11/06/25
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
